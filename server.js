333b7c8b47e6344720dfbe1f3e6c076c7df41789
Monu Kumar Jha <monu270202@gmail.com>
2025-08-05 20:00:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on("connection", (socket) => {
     console.log("User disconnected:", socket.id);
   });
 });
-
-server.listen(5000, () => {
-  console.log("✅ Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
